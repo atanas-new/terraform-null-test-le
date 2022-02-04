c80f59857b173024198934b3cc510ee4729ba503
Atanas Chuchulev <atanas.v4@gmail.com>
2022-02-04 16:55:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ resource "null_resource" "le" {
   }
   
   provisioner "local-exec" {
-    command = "pwd && ls -la && whoami && sleep 10"
+    #command = "pwd && ls -la && whoami && sleep 10"
+    command = "python3 -V; pip3 --version;"
   }
   
 }
